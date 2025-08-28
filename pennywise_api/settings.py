f140b40ba422f729cf4792b4dd8cb3cd07af5177
Deepak Rajamohan <iamdpak@gmail.com>
2025-08-28 17:14:17 +1000

    s3 bucket uploads

--- pennywise_api/settings.py
+++ pennywise_api/settings.py
@@ -116,9 +116,7 @@ TEMPLATES = [
 
 
 # storage
-INSTALLED_APPS += ["storages"]
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = ".backends.s3boto3.S3Boto3Storage"
 AWS_S3_ENDPOINT_URL = "http://minio:9000"
 AWS_ACCESS_KEY_ID = "minioaccess"
 AWS_SECRET_ACCESS_KEY = "miniosecret"
